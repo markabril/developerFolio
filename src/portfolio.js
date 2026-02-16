2aa38cfe913648bace30731ba462dc1121ffbbca
maru <mabril@egcextremeunrealtechnology.com>
2026-02-16 16:09:20 +0800

    asdf

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,7 +21,7 @@ const illustration = {
 
 const greeting = {
   username: "Mark Anthony Abril",
-  title: "Hi all, I'm Mark",
+  title: "Hi all, I'm Mark Anthony",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Nodejs and some other cool libraries and frameworks."
   ),
